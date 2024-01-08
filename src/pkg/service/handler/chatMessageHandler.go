27b5e01c56d939d447f35b4bd833a28603268721
Niel Müller <niel.mueller@mgb.ch>
2024-01-08 13:35:04 +0100

    changed to not need name

--- src/pkg/service/handler/chatMessageHandler.go
+++ src/pkg/service/handler/chatMessageHandler.go
@@ -11,26 +11,25 @@ type message struct {
 	Content map[string]string `json:"content"`
 }
 
-func MessageHandlerChats(msg []byte, name string) {
-	if name != "" {
-		// Create an empty Message object
-		var message message
-		// Unmarshal the JSON string into the Message object
-		err := json.Unmarshal(msg, &message)
-		if err != nil {
-			fmt.Println("Error:", err)
-		} else {
-			// Print the Message object
-			switch message.Typ {
-			case "sendmessage":
-				//sendmessage(message.Content)
-			case "getmessage":
-				//getmessage(message.Content)
-			case "getoverview":
-				//getoverview(message.Content)
-			case "getmembers":
-				//getmembers(message.Content)
-			}
+func MessageHandlerChats(msg []byte) {
+	// Create an empty Message object
+	var message message
+	// Unmarshal the JSON string into the Message object
+	err := json.Unmarshal(msg, &message)
+	if err != nil {
+		fmt.Println("Error:", err)
+	} else {
+		switch message.Typ {
+		case "login":
+			//login(message.Content);
+		case "sendmessage":
+			//sendmessage(message.Content)
+		case "getmessage":
+			//getmessage(message.Content)
+		case "getoverview":
+			//getoverview(message.Content)
+		case "getmembers":
+			//getmembers(message.Content)
 		}
 	}
 }
